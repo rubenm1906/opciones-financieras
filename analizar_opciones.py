d07426cb562e233fa078b5812a0f38dcb7f7e4f6
rubenm1906 <rubenmarin19@gmail.com>
2025-03-15 13:38:37 +0100

    Update analizar_opciones.py

--- analizar_opciones.py
+++ analizar_opciones.py
@@ -5,15 +5,15 @@ from tabulate import tabulate
 
 # Configuración
 TICKERS = list(set(["TEP.PA", "GLNG", "GOOGL", "EPAM", "NA9.DE", "NVDA"]))  # Aseguramos que no haya duplicados
-MIN_RENTABILIDAD_ANUAL = 10
+MIN_RENTABILIDAD_ANUAL = 30
 MAX_DIAS_VENCIMIENTO = 30  # Filtro máximo de 90 días
 MIN_DIFERENCIA_PORCENTUAL = 5  # Filtro mínimo para la diferencia % (Subyacente - Break-even)
 MIN_VOLUMEN = 10  # Filtro mínimo de volumen
 MIN_VOLATILIDAD_IMPLÍCITA = 20  # Mínimo de volatilidad implícita en %
 MAX_VOLATILIDAD_IMPLÍCITA = 50  # Máximo de volatilidad implícita en %
-MIN_OPEN_INTEREST = 10  # Mínimo de interés abierto
+MIN_OPEN_INTEREST = 20  # Mínimo de interés abierto
 FILTRO_TIPO_OPCION = "OTM"  # Opciones: "OTM", "ITM", "TODAS"
-TOP_CONTRATOS = 5  # Número de contratos a mostrar en la tabla de "Mejores Contratos"
+TOP_CONTRATOS = 10  # Número de contratos a mostrar en la tabla de "Mejores Contratos"
 
 # Variable para evitar ejecuciones múltiples
 SCRIPT_EJECUTADO = False
